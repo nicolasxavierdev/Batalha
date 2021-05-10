9a04cf4f01f818e56b214ff1c86120893f4ed25b
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-05-10 13:39:31 -0300

    fim de jogo

--- JS/main.js
+++ JS/main.js
@@ -3,15 +3,22 @@ let energiaInimigo = 100;
 
 const atacarHeroi = () => {
     energiaHeroi = energiaHeroi - retornaNumeroAleatorio();
+
+    if (energiaHeroi <= 0) {
+        alert('fim de jogo, Inimigo venceu.')
+    }
     console.log('Herói ficou com: ', energiaHeroi);
 }
 
 const atacarInimigo = () => {
     energiaInimigo = energiaInimigo - retornaNumeroAleatorio();
+
+    if (energiaInimigo <= 0) {
+        alert('fim de jogo, Herói venceu.')
+    }
     console.log('Inimigo ficou com: ', energiaInimigo);
 }
 
 const retornaNumeroAleatorio = () => {
     return Math.floor(Math.random() * 11);
 }
-
