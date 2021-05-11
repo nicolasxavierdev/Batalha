bcf1dc5488994998ac5c85198000809f05ef8931
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2021-05-11 12:09:21 -0300

    desabilitar botão

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,35 @@
 let energiaHeroi = 100;
 let energiaInimigo = 100;
+let energiaIni = document.getElementById('energiaI')
+let energiaHer = document.getElementById('energiaH')
 
 const atacarHeroi = () => {
     energiaHeroi = energiaHeroi - retornaNumeroAleatorio();
 
-    document.getElementById('energiaI').innerHTML = energiaHeroi;
+    energiaIni.innerHTML = energiaHeroi;
 
     if (energiaHeroi <= 0) {
         alert('fim de jogo, Inimigo venceu.')
+        desabitaBotao()
     }
 }
 
 const atacarInimigo = () => {
     energiaInimigo = energiaInimigo - retornaNumeroAleatorio();
 
-    document.getElementById('energiaH').innerHTML = energiaInimigo;
+    energiaHer.innerHTML = energiaInimigo;
 
     if (energiaInimigo <= 0) {
         alert('fim de jogo, Herói venceu.')
+        desabitaBotao()
     }
 }
 
 const retornaNumeroAleatorio = () => {
     return Math.floor(Math.random() * 11);
+}
+
+const desabitaBotao = () => {
+    document.getElementById('bI').disabled = true;
+    document.getElementById('bH').disabled = true;
 }
